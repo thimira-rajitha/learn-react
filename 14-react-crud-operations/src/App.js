553a36d160bda86785d73ca1f571aa149b8d7d81
Thimira Rajitha <Rajitha@dmsswe.com>
2023-01-19 05:43:10 +0530

    react CRUD complete

--- 14-react-crud-operations/src/App.js
+++ 14-react-crud-operations/src/App.js
@@ -42,24 +42,39 @@ function App() {
     const postOptions = {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(myNewItem)
-    }
+      body: JSON.stringify(myNewItem),
+    };
 
     const result = await apiRequest(API_URL, postOptions);
     if (result) setFetchError(result);
-
   };
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item));
     setItems(listItems);
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE' };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   };
 
   const handleSubmit = (e) => {
